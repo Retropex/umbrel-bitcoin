01f9444799726a1c302ca1eba7d3ff0a6715d66a
Léo Haf <leo@haf.ovh>
2026-02-11 09:49:47 +0000

    Add Knots v29.3.knots20260210

--- libs/settings/settings.meta.ts
+++ libs/settings/settings.meta.ts
@@ -8,7 +8,7 @@
 // IMPORTANT:
 // - Any version added here needs to be added in the Dockerfile
 // - The array of versions must be newest → oldest. We do a simple index comparison to compare versions, so lower index = newer.
-export const AVAILABLE_BITCOIN_KNOTS_VERSIONS = ['v29.2.knots20251110', 'BIP110','v29.2', 'v29.1'] as const
+export const AVAILABLE_BITCOIN_KNOTS_VERSIONS = ['v29.3.knots20260210', 'v29.2.knots20251110', 'BIP110','v29.2', 'v29.1'] as const
 
 // Default Bitcoin Knots version used by bitcoind manager (always the newest version in the array)
 export const DEFAULT_BITCOIN_KNOTS_VERSION = AVAILABLE_BITCOIN_KNOTS_VERSIONS[0]
@@ -363,6 +363,7 @@ export const settingsMetadata = {
 		unit: 'bytes',
 		versionOverrides: {
 			// v29.2.knots20251110 changed the default and max so we declare a tiny diff
+			'v29.3.knots20260210': {default: 83},
 			'v29.2.knots20251110': {default: 83},
 			'BIP110': {default: 83},
 		},
