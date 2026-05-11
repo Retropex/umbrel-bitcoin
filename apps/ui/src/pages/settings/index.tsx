94a471057775741a0ea8b4e013fc28514f11d68e
Léo Haf <leo@haf.ovh>
2026-05-11 13:20:26 +0200

    add new info field and reword the RDTS warning

--- apps/ui/src/pages/settings/index.tsx
+++ apps/ui/src/pages/settings/index.tsx
@@ -200,6 +200,7 @@ function FieldRenderer({
 				</div>
 				<p className='text-[13px] font-[400] text-white/60'>{option.description}</p>
 				{option.subDescription && <p className='text-[12px] font-[400] text-white/60 mt-1'>{option.subDescription}</p>}
+				{option.info && <p className='text-[12px] font-[400] text-white/60 mt-1'>{option.info}</p>}
 				<p className='text-[12px] font-[400] text-white/50  mt-2'>
 					default: {option.default} {option.unit}
 				</p>
@@ -246,6 +247,9 @@ function FieldRenderer({
 						<p className='text-[13px] font-[400] text-white/60'>{option.description}</p>
 						{option.subDescription && (
 							<p className='text-[12px] font-[400] text-white/60 mt-1'>{option.subDescription}</p>
+						)}
+						{option.info && (
+							<p className='text-[12px] font-[400] text-white/60 mt-1'>{option.info}</p>
 						)}
 						<p className='text-[12px] font-[400] text-white/50 mt-2'>
 							default: {option.default ? 'enabled' : 'disabled'}
@@ -310,6 +314,9 @@ function FieldRenderer({
 							<p className='text-[13px] font-[400] text-white/60'>{option.description}</p>
 							{option.subDescription && (
 								<p className='text-[12px] font-[400] text-white/60 mt-1'>{option.subDescription}</p>
+							)}
+							{option.info && (
+								<p className='text-[12px] font-[400] text-white/60 mt-1'>{option.info}</p>
 							)}
 							<p className='text-[12px] font-[400] text-white/50 mt-2'>
 								default: {option.default.length ? option.default.join(', ') : 'none'}
@@ -380,6 +387,9 @@ function FieldRenderer({
 						<p className='text-[13px] font-[400] text-white/60'>{option.description}</p>
 						{option.subDescription && (
 							<p className='text-[12px] font-[400] text-white/60 mt-1'>{option.subDescription}</p>
+						)}
+						{option.info && (
+							<p className='text-[12px] font-[400] text-white/60 mt-1'>{option.info}</p>
 						)}
 						<p className='text-[12px] font-[400] text-white/50 mt-2'>default: {option.default}</p>
 					</div>
